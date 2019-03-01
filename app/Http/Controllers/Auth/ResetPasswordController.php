26c07432a41b9af5fe736294a9151c8334ce436b
hanyaokun <448179430@qq.com>
2019-03-01 10:49:59 +0800

    修改密码 验证提醒

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Zend\Diactoros\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +36,9 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+    protected function sendResetResponse(Request $request,$response){
+        session()->flash('success','密码更新成功,您已登陆成功');
+        return redirect($this->redirectPath());
     }
 }
