f314296f568b478551d205075b75558d7dd68831
hanyaokun <448179430@qq.com>
2019-03-01 10:11:53 +0800

    注册，验证码，邮箱验证 修复几个小BUG

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailsIsVerified::class,  //注册邮箱验证中间件
         ],
 
         'api' => [
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,8 +9,28 @@
 
             </ul>
             <ul class="navbar-nav navbar-right">
-                <li class="navbar-item"><a href="#" class="nav-link">登陆</a></li>
-                <li class="navbar-item"><a href="#" class="nav-link">注册</a></li>
+                @guest
+                <li class="navbar-item"><a href="{{route('login')}}" class="nav-link">登陆</a></li>
+                <li class="navbar-item"><a href="{{route('register')}}" class="nav-link">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="">个人中心</a>
+                            <a class="dropdown-item" href="">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" id="logout" href="#">
+                                <form action="{{ route('logout') }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                </form>
+                            </a>
+                        </div>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
