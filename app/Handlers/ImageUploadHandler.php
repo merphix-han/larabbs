cc26283d61559c073d28c479991879861188c1f9
hanyaokun <448179430@qq.com>
2019-03-04 13:46:51 +0800

    头像改变大小 User::edit AND update 权限限制

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -6,10 +6,10 @@
  * Time: 上午 11:16
  */
 namespace App\Handlers;
-
+use Image;
 class ImageUploadHandler{
-    protected $allowed_ext = ['png','jpg','jpeg','jpg'];
-    public function save($file,$folder,$file_prefix)
+    protected $allowed_ext = ['png','jpg','jpeg','gif'];
+    public function save($file,$folder,$file_prefix,$max_width=false)
     {
         $folder_name = "uploads/images/$folder/".date('Ym/d'.time());
 
@@ -23,9 +23,24 @@ class ImageUploadHandler{
             return false;
         }
         $file->move($upload_path,$file_name);
+        if($max_width&&$extension!='jpg')
+        {
+            $this->reduceSize($upload_path.'/'.$file_name,$max_width);
+        }
         return [
             'path'=>config('app.url')."/$folder_name/$file_name",
         ];
 
+    }
+    public function reduceSize($file_path,$max_width)
+    {
+        $image = Image::make($file_path);
+        $image->resize($max_width,null,function($constraint){
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+            //防止截图时图片尺寸变大
+            $constraint->upsize();
+        });
+        $image->save();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,17 +8,24 @@ use App\Models\User;
 use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     //
     public function show(User $user){
         return view('users.show',compact('user'));
     }
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar){
-            $result = $uploader->save($request->avatar,'avatar',$user->id);
+            $result = $uploader->save($request->avatar,'avatar',$user->id,416);
             if($result){
                 $data['avatar'] = $result['path'];
             }
